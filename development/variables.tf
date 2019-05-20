7100e90ead0906eddf72638a83b18e6db7ee6d71
Pat Kaehuaea <pkaehuae@starbucks.com>
2019-05-19 22:17:54 -0700

    Ordered alphabetically and added terraform_backend_config attributes for module.

--- development/variables.tf
+++ development/variables.tf
@@ -1,20 +1,27 @@
-variable "key" {
-	"default" = "dev/terraform.state"
+variable "container_name" {
+    "default" = "cnhomedev01"
 }
 
-variable "resource_group_name" {
-	"default" = "rghomedev01"
+variable "key" {
+    "default" = "dev/terraform.state"
 }
 
 variable "location" {
-	"default" = "westus"
+    "default" = "westus"
+}
+
+variable "resource_group_name" {
+    "default" = "rghomedev01"
 }
 
 variable "storage_account_name" {
-	"default" = "sahomedev01"
+    "default" = "sahomedev01"
 }
 
-variable "container_name" {
-	"default" = "cnhomedev01"
+variable "teraform_backend_config_file_path" {
+    "default" = "../modules/terraform_state_backend/templates/"
 }
 
+variable "terraform_backend_config_file_name" {
+    "default" = "terraform.tf"
+}
