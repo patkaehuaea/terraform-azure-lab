40c7413ca9ba1b686214ade8a1c704626f7b72e1
Pat Kaehuaea <pkaehuae@starbucks.com>
2019-05-19 22:24:05 -0700

    Replaced multiple resources to single module reference.

--- development/main.tf
+++ development/main.tf
@@ -1,36 +1,13 @@
 provider "azurerm" {
 }
-resource "azurerm_resource_group" "rg" {
-        name = "${var.resource_group_name}"
-        location = "${var.location}"
-}
 
-resource "azurerm_storage_account" "sa" {
-        name = "${var.storage_account_name}"
-        resource_group_name = "${azurerm_resource_group.rg.name}"
-        location = "${var.location}"
-        account_tier = "Standard"
-        account_replication_type = "LRS"
-}
-
-resource "azurerm_storage_container" "sc" {
-        name = "${var.container_name}"
-        resource_group_name = "${azurerm_resource_group.rg.name}"
-        storage_account_name = "${azurerm_storage_account.sa.name}"
-        container_access_type = "private"
-}
-
-data "template_file" "terraform_backend_config" {
-  template = "${file("../modules/terraform_state_backend/templates/terraform.tf.tpl")}"
-  vars {
-	resource_group_name = "${azurerm_resource_group.rg.name}"
-    	storage_account_name = "${azurerm_storage_account.sa.name}"
-	container_name = "${var.container_name}"
-	key = "${var.key}"	
-  }
-}
-
-resource "local_file" "terraform_backend_config" {
-  content  = "${data.template_file.terraform_backend_config.rendered}"
-  filename = "./terraform.tf"
+module "terraform_state_backend" {
+    source = "../modules/terraform_state_backend"
+    container_name = "${var.container_name}"
+    key = "${var.key}"
+    location = "${var.location}"
+    resource_group_name = "${var.resource_group_name}"
+    storage_account_name = "${var.storage_account_name}"
+    terraform_backend_config_file_path = "${var.terraform_backend_config_file_path}"
+    terraform_backend_config_file_name = "${var.terraform_backend_config_file_name}"
 }
