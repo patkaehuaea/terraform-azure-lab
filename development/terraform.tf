585959bbdcf22a0914fd4143e08dd836e362d8ed
Pat Kaehuaea <pkaehuae@starbucks.com>
2019-05-21 12:58:31 -0700

    Adding makefile in common directory with symlinks in environment. The makefile automates bootstrapping of the remote-backend.

--- development/terraform.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-	backend "azurerm" {
-		resource_group_name = "rghomedev01"
-		storage_account_name = "sahomedev01"
-		container_name = "cnhomedev01"
-		key = "dev/terraform.state"
-	}
-}
