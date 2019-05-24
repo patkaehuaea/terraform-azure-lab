4165ca80853c9095c0015cffd56b8563f258ca20
Pat Kaehuaea <pkaehuae@starbucks.com>
2019-05-24 00:14:33 -0700

    Missed the diff but nothing too drastic.

--- modules/common/network/virtual_network/output.tf
+++ modules/common/network/virtual_network/output.tf
@@ -14,6 +14,10 @@ output "subnet_name" {
     "value" = "${var.subnet_name}"
 }
 
+output "subnet_id" {
+    "value" = "${azurerm_subnet.default.id}"
+}
+
 output "vnet_name" {
     "value" = "${var.vnet_name}"
 }
