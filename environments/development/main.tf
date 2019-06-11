1b7eb38279e6dc17e27214aee0cbf6dc2a79dc6f
Pat Kaehuaea <pkaehuae@starbucks.com>
2019-06-10 22:26:56 -0700

    Blind commit :(

--- environments/development/main.tf
+++ environments/development/main.tf
@@ -9,8 +9,8 @@ module "virtual_network" {
   address_prefix      = "${var.address_prefix}"
   address_space       = "${var.address_space}"
   location            = "${var.location}"
-  resource_group_name = "${var.resource_group_name}"
-  source              = "../../../modules/common/network/virtual_network/"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+  source              = "../../modules/base/network/virtual_network/"
   subnet_name         = "${var.subnet_name}"
   vnet_name           = "${var.vnet_name}"
 }
@@ -18,14 +18,14 @@ module "virtual_network" {
 module "key_vault" {
   key_vault_name      = "kvdevcore01"
   location            = "${var.location}"
-  resource_group_name = "${var.resource_group_name}"
-  source              = "../../../modules/common/storage/key_vault/"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+  source              = "../../modules/base/storage/key_vault/"
 }
 
 module "certificate" {
   cert_name    = "${var.cert_name}"
   key_vault_id = "${module.key_vault.key_vault_id}"
-  source       = "../../../modules/common/certificate/"
+  source       = "../../modules/base/certificate/"
 }
 
 data "azurerm_key_vault_secret" "default" {
@@ -39,6 +39,6 @@ module "application_gateway" {
   cert_value               = "${data.azurerm_key_vault_secret.default.value}"
   location                 = "${var.location}"
   resource_group_name      = "${var.resource_group_name}"
-  source                   = "../../../modules/common/application_gateway/"
+  source                   = "../../modules/services/application_gateway/"
   subnet_id                = "${module.virtual_network.subnet_id}"
 }
